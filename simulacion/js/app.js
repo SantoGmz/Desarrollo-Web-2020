ce2ed4fb88bd41a4666598b9b8aee2d4fb5d7fc2
S4N70 <46525658+SantoGmz@users.noreply.github.com>
2020-03-24 04:09:48 -0400

    Generando variables de parametros
    Generando las variables que usaré para trabajar, las cuales son los parametros de la simulacion.

--- simulacion/js/app.js
+++ simulacion/js/app.js
@@ -4,11 +4,27 @@ function reloj() {
     var minutos = fecha.getMinutes();
     var segundos = fecha.getSeconds();
     var actualizacion = setTimeout('reloj()', 500);
+
     document.getElementById('hora').innerHTML= hora + " :";
     document.getElementById('minutos').innerHTML=  minutos + " :";
     document.getElementById('segundos').innerHTML=  segundos;
 
 }
+
+
+var tiempoJugado = document.getElementById('tiempoJugado');
+
+var numeroDeMaquinas = document.getElementById('numeroDeMaquinas');
+
+var loterias = document.getElementById('loterias');
+
+var cantidaDeJugadores = document.getElementById('cantidaDeJugadores');
+
+var cantidadDeJugadas = document.getElementById('cantidadDeJugadas');
+
+var cantidadDeDinerojugado = document.getElementById('cantidadDeDinerojugado');
+
+
 
 
 
