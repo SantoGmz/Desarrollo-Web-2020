0543b65d79e29454b4065ab8f625bafdf096d359
S4N70 <46525658+SantoGmz@users.noreply.github.com>
2020-03-25 00:00:40 -0400

    Numeros aleatorios

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,25 @@ function generarAleatorio(minimo, maximo) {
     return Math.floor(Math.random() * (maximo - minimo) + minimo);
 }
 
+function aleatoria(x) {
+    return Math.floor(Math.random() * x);
+}
+
 for (let i = 1; i < 100; i++) {
-    document.write('Linea numero ' +i+' Numero Generado: '+generarAleatorio(1,100) + "<br>");
+    //document.write('Linea numero ' +i+' Numero Generado: '+generarAleatorio(1,100) + "<br>");
     
 }
+
+
+
+
+
+let arr= [];
+for(var i =1;i<=100;i++){
+arr.push(i);
+}
+
+var d = arr[aleatoria(101)]
+document.write(arr[aleatoria(101)] + "<br>");
+document.write(arr[aleatoria(101)] + "<br>");
+document.write(arr[aleatoria(101)] + "<br>");
