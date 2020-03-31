601329c64e9f457ca61ec1c6cb170edeb89702ae
S4N70 <mr.sansan777@gmail.com>
2020-03-31 15:15:12 -0400

    Metodo ready
    Con el metodo ready, aprendi que este metodo tiene 4 formas diferentes de usar, y que este ejecuta codigo cuando esta totalmente cargada la pagina con
    $(document).ready(function(){
    alert("Este codigo sera el que se imprime cuando este totalmente cargada la pagina");
    });
    
    y tambien que la forma recomendada es,
    $(function(){
    alert("Esta es la forma mas recomendada.");
    });
    
    Tambien podemos seleccionar con
    $('.clase').ready(funtion(){
    alert("ejem");
    });

--- jqueryPracticas/v-4/js/script.js
+++ jqueryPracticas/v-4/js/script.js
@@ -1,1 +1,23 @@
-alert("Hola.");
+/*
+//1ra forma de usar el metodo ready
+$(function(){
+    alert("Hola.");
+});
+
+//2da forma de usar el metodo ready
+$().ready(function(){
+    alert("Segunda forma.");
+});
+
+
+3ra forma
+$(document).ready(function(){
+    
+    $('.titulo').text("Cambiado");
+});
+*/
+
+//4ta forma y la mas recomendada, esta hace que el documento este totalmente cargado.
+$(function(){
+alert('La pagina esta totalmente cargada');
+});
