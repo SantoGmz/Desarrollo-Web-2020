c27b74a9434ec4c7a434a3495cc8907d075d03d5
S4N70 <mr.sansan777@gmail.com>
2020-03-31 13:55:39 -0400

    Videos de codigo facilito
    practicas e jquery, nunca use esta vaiina

--- simulacion/js/app.js
+++ simulacion/js/app.js
@@ -4,47 +4,38 @@ function reloj() {
     var minutos = fecha.getMinutes();
     var segundos = fecha.getSeconds();
     var actualizacion = setTimeout('reloj()', 500);
-
     document.getElementById('hora').innerHTML= hora + " :";
     document.getElementById('minutos').innerHTML=  minutos + " :";
     document.getElementById('segundos').innerHTML=  segundos;
-
 }
-
-
 
 //botones
 const btninicio = document.querySelector('#btninicio')
 
 btninicio.addEventListener('click', ()=>{
-    
 
     //horas que jugara cada persona
    // var tiempoJugado = document.getElementById('tiempoJugado');
-    var tiempoJugado = document.querySelector('#tiempoJugado');
+    var tiempoJugado = document.querySelector('#tiempoJugado').value;
 
     //lo multiplico por 60 por que seran horas x minutos
     //var tiempoJugado = tiempoJugado*60;
         
     //numero de terminales/maquinas que seran abiertas para jugadas
-    var numeroDeMaquinas = parseInt(document.getElementById('numeroDeMaquinas'));
+    var numeroDeMaquinas = document.querySelector('#numeroDeMaquinas').value;
 
     //Numero de loterías cuales jugará
-    var loterias = document.getElementById('loterias');
+    var loterias = document.querySelector('#loterias').value;    
 
     //jugadores que Habran
-    var cantidaDeJugadores = document.getElementById('cantidaDeJugadores');
+    var cantidaDeJugadores = document.querySelector('#cantidaDeJugadores').value;    
 
     //cantidad de quinielas
-    var cantidadDeJugadas = document.getElementById('cantidadDeJugadas');
+    var cantidadDeJugadas = document.querySelector('#cantidadDeJugadas').value;    
 
     //dinero que jugará cada quien
-    var cantidadDeDinerojugado = document.getElementById('cantidadDeDinerojugado');
+    var cantidadDeDinerojugado = document.querySelector('#cantidadDeDinerojugado').value;    
 
-
-
-return console.log(tiempoJugado.value);
-
-
+    return console.log(tiempoJugado + numeroDeMaquinas + loterias + cantidaDeJugadores + cantidadDeJugadas + cantidadDeDinerojugado);
 
   });
