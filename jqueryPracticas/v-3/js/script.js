f24766263d5de3ada62efbba62fa4cdc2a0b52b1
S4N70 <mr.sansan777@gmail.com>
2020-03-31 14:11:32 -0400

    Selectores Basicos.
    Seleccione etiquetas de html, id, clases y cambié  el css, tambien el texto de esas etiquetas

--- jqueryPracticas/v-3/js/script.js
+++ jqueryPracticas/v-3/js/script.js
@@ -1,1 +1,14 @@
-alert("Hola.");
+
+
+
+$(document).ready(function(){
+   $('#parrafo').text("Parrafo cambiado");
+   $('#parrafo').css({'color':'red'});
+
+   $('.titulo').text("Titulo Cambiado.");
+   $('.titulo').css({'color':'red'});
+
+   $('div').text("Hola, ¿Como estas?");
+
+});
+
