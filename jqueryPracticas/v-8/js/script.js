f5a60ab4ed65f077a2f5884a604fae61d08e2df7
S4N70 <mr.sansan777@gmail.com>
2020-04-01 16:18:48 -0400

    Menejo de clases
    .HasClass(), Puede ser utilizado para comprobar si tiene una clase
    
    addClass():Para añadir clases css
    
    removeClass(): Para borrar clases css

--- jqueryPracticas/v-8/js/script.js
+++ jqueryPracticas/v-8/js/script.js
@@ -1,1 +1,11 @@
-alert("Hola.");
+$(function(){
+
+  if (  $('#parrafo').hasClass('saludo')) {
+        alert("si tiene la clase saludo");
+        $('#parrafo').removeClass('saludo');
+        
+    }else{
+        alert("No tiene la clase saludo");
+        $('p').addClass('saludo');
+    }
+});     
