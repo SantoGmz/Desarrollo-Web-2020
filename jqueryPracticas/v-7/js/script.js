aebba8b7d69836742bda85d5d2886f21620da5f8
S4N70 <mr.sansan777@gmail.com>
2020-04-01 14:40:47 -0400

    Metodo css
    aprendiedo a usar css con jquery
    
    sintaxis
    $('.ejem').css({'color':'red','background-color':'black'});
    
    primero selecciono la etiqueta a trabajar $('.ejem'), Luego uso el metodo .css() dentro de los parentesis con llaves {} y entre comillas '' '' pongo la regla css y su valor algo como ({'color':'red'}); , pero tambien, si quiero poner mas reglas css puedo dividirlo con una ,(coma) ejemplo.
    ({'color':'red','background-color':'black'});

--- jqueryPracticas/v-7/js/script.js
+++ jqueryPracticas/v-7/js/script.js
@@ -1,1 +1,7 @@
-alert("Hola.");
+$(function(){
+    $('.titulo').css({'background-color':'red','color':'white'})
+
+    $('.titulo').click(function(){
+        $('.titulo').css({'background-color':'white', 'color':'black'});
+    });
+});
