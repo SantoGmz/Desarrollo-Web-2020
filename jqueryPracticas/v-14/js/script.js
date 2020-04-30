9394558667dcd0d58f51a68601caf2384f0549c0
sotnas <mr.sansan777@gmail.com>
2020-04-30 00:11:37 -0400

    uso de json

---
+++ jqueryPracticas/v-14/js/script.js
@@ -0,0 +1,35 @@
+let objeto={ 
+    nombre:{
+        PrimerNombre:"Santos",
+        segundoNombre:"Severino",
+        apellido:"Gomez"
+    },
+    edad:20
+};
+
+
+const unJson=[
+    { id: "1", valor: "texto1"},
+    { id: "3", valor: "texto2"},
+    { id: "5", valor: "texto1"}
+  ];
+
+
+
+
+
+const otroJson = [
+    {nombre: "Santos", apellido: "Gomez"},
+    {nombre: "Jose", apellido: "Favian"},
+    {nombre: "Sotnas", apellido: "Gomez"},
+    {nombre: "Alex", apellido: "Grezzy"},
+]
+
+const objeto3 = {
+    nombre:"jose",
+    
+}
+const json3 = [{}]
+
+console.log(objeto.nombre.PrimerNombre)  ;
+console.log(unJson[0].valor)  ;
